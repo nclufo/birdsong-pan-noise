e132cd3a347fc8727b5bcc419487ef821c886243
nclufo <naomi.clough13@gmail.com>
2025-01-13 12:23:59 +0000

    mouse-seeking boid flock

--- sketch.js
+++ sketch.js
@@ -1,45 +1,61 @@
 let birdsong;
 let panning;
 let vol;
+let cnv;
+let vehicles = [] ;
 
 function preload(){
-  birdsong = loadSound('birdsong.mp3');
+  birdsong = loadSound('sound/birdsong.mp3');
 }
 
 function setup() {
-  let cnv = createCanvas(500, 500);
+  cnv = createCanvas(500, 500);
   cnv.mousePressed();
+  
+    for (let i = 0; i < 50; i++) {
+    vehicles.push(new Vehicle(random(width), random(height)));
+  }
 }
 
 function draw() {
   background(255);
-  let t = frameCount * 0.002;
+      let t = frameCount * 0.002;
   noStroke();
   for (let i = 0; i < 500; i += 5) {
     for (let j = 0; j < 500; j += 5) {
       var n = noise(i * 0.005, j * 0.005 - t, t);
-      fill(n*230, n*240, n * 250, 80);
+      fill(n*230, n*240, n * 250, 90);
       rect(i, j, 5);
     }
   }
+  
   let noiseLevel = 500;
   let noiseScale = 0.005;
-  
+
   let nt = noiseScale * frameCount;
-  
+
   let x = noiseLevel * noise(nt);
   let y = noiseLevel * noise(nt + 10000);
   
-  fill(0);
-  strokeWeight(15);
-  rect(x, y, 10, 10);
+  fill(15);
+  noStroke();
+  circle(x, y, 10);
   
   panning = map(x, 0, 500, -1, 1);
   birdsong.pan(panning);
+
   
-  vol = map(y, 0, 500, 1, 0.2);
+  vol = map(y, 0, 500, 1, 0);
   birdsong.setVolume(vol);
   console.log(panning, vol);
+  
+  
+    for (let v of vehicles) {
+    v.applyBehaviors(vehicles);
+    v.update();
+    v.borders();
+    v.show();
+  }
 }
 
 function mousePressed() {
@@ -48,5 +64,4 @@ function mousePressed() {
  } else {
    birdsong.pause();
 }
-
 }
